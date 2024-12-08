79cc10640e871b0d3a60d1e0671bfd7ae70c6f1f
ivanovaa <ivanovaa@mir-omsk.ru>
2024-12-08 15:50:48 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NativeBaseProvider, Center } from 'native-base';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center width="100%" height="100%"> s fsd fsddfs 222</Center>
+    </NativeBaseProvider>
+  );
+}
