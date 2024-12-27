8a2683dd2ea13176f4cc4bcef1c5d7410f32de86
ivanovaa <ivanovaa@mir-omsk.ru>
2024-12-27 21:07:59 +0600

    firestore

--- content-builder-web-app/src/app/app.component.ts
+++ content-builder-web-app/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { AppIconsRegistry } from '@/core/utils';
 import { ToggleLightDarkButtonComponent } from '@/core/components';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatButton } from '@angular/material/button';
+import { uniqueId } from 'lodash';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [
-    ToggleLightDarkButtonComponent
+    ToggleLightDarkButtonComponent,
+    MatButton,
+    JsonPipe
   ],
   host: {
     class: 'block w-full h-full relative'
@@ -14,10 +22,26 @@ import { ToggleLightDarkButtonComponent } from '@/core/components';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly _firestore = inject(AngularFirestore);
+  private readonly _folders = this._firestore.collection('folders');
   private readonly _iconsRegistry = inject(AppIconsRegistry);
-  title = 'content-builder-web-app';
+
+  protected readonly _foldersList = toSignal(this._folders.snapshotChanges()
+    .pipe(map(actions => actions.map(action => ({
+      id: action.payload.doc.get('id'),
+      name: action.payload.doc.get('name')
+    })))));
 
   constructor() {
     this._iconsRegistry.addAllSvgIcons();
+    this._folders.get()
+      .subscribe(async (actions) => {
+        const data = await actions.query.get();
+        console.info(data.docs.map(doc => doc.data()));
+      });
+  }
+
+  _handleAdd() {
+    void this._folders.add({ id: uniqueId('folders'), name: 'Folder 1' });
   }
 }
--- content-builder-web-app/src/app/app.config.ts
+++ content-builder-web-app/src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDcUJsYrKgs0usrFVNa0wrqDenVqYZPXFI',
+      authDomain: 'urgent-care-9444e.firebaseapp.com',
+      projectId: 'urgent-care-9444e',
+      storageBucket: 'urgent-care-9444e.firebasestorage.app',
+      messagingSenderId: '968549272760',
+      appId: '1:968549272760:web:88d791e2e2c1291a03b84d',
+      measurementId: 'G-66VELK0HS7'
+    })),
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)]
