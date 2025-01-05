69b746770445a21434be73bd16ed7812c8b09e36
ivanovaa <ivanovaa@mir-omsk.ru>
2025-01-05 21:50:09 +0600

    fix

--- content-builder-web-app/src/app/app.routes.ts
+++ content-builder-web-app/src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     pathMatch: 'full',
     path: '',
     loadComponent: () => import('./core/components/folders-explorer/folders-explorer.component').then(x => x.FoldersExplorerComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
