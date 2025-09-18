51d060904cba9a42bf560b89ddce8d873297199b
cubersport12 <99469564+cubersport12@users.noreply.github.com>
2025-09-18 11:56:11 +0600

    fix: починил токены к базе

--- mobile-app/supabase.ts
+++ mobile-app/supabase.ts
@@ -3,8 +3,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cbnvjqjrsyrpnopspwzk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNibnZqcWpyc3lycG5vcHNwd3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzODQ4NDYsImV4cCI6MjA1MDk2MDg0Nn0.MQVBPozROqV0vUpfLQoG8itS8PqChZI2rCzolieB4eI';
+const supabaseUrl = 'https://jjqzkpedbpfytjotaank.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqcXprcGVkYnBmeXRqb3RhYW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzNjI5MDMsImV4cCI6MjA3MDkzODkwM30.PKjoTccGMVYiuvWqvHoebCa0CPmlHd74a5s9P4mssJc';
 
 class SupabaseStorage {
   async getItem(key: string) {
