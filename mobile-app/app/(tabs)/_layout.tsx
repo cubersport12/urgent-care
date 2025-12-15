7efc6119f3bdb9bfffd15efbed12df76daa5d5f9
cubersport12 <99469564+cubersport12@users.noreply.github.com>
2025-12-15 18:22:17 +0600

    feat: вернул react native

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,55 +1,33 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Icon } from '@/components/ui/icon';
-import { House, UserRound } from 'lucide-react-native';
+import { HapticTab } from '@/components/haptic-tab';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
   return (
     <Tabs
-      initialRouteName="(explorer)"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        tabBarButton: HapticTab,
         headerShown: false,
-        tabBarBackground: TabBarBackground,
-        tabBarIconStyle: Platform.select({
-          default: {
-          }
-        }),
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute'
-          },
-          default: {
-          }
-        })
-      }}
-    >
-      <Tabs.Screen name="index" options={{ tabBarItemStyle: { display: 'none' } }} />
+        tabBarButton: HapticTab,
+      }}>
       <Tabs.Screen
-        name="(explorer)"
+        name="index"
         options={{
-          tabBarHideOnKeyboard: true,
-          href: '/',
-          headerTitle: 'Главная',
-          tabBarLabel: 'Главная',
-          tabBarIcon: ({ color }) => <Icon as={House} size="xl" color={color} />
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="profile"
+        name="explore"
         options={{
-          headerTitle: 'Профиль',
-          tabBarLabel: 'Профиль',
-          tabBarIcon: ({ color }) => <Icon as={UserRound} size="xl" color={color} />
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,72 +1,24 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
-import '../global.css';
-import LoginScreen from './screens/LoginScreen';
-import { SafeAreaView } from 'react-native';
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { supabase } from '@/supabase';
-import { Session } from '@supabase/supabase-js';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-void SplashScreen.preventAutoHideAsync();
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
 
 export default function RootLayout() {
-  const [session, setSession] = useState<Session | null | undefined>(undefined);
-  const colorScheme = 'dark';
-  const [loaded] = useFonts({
-
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
-  });
-
-  const onAuthStateChanged = (user: Session | null) => {
-    setSession(user);
-  };
-
-  useEffect(() => {
-    void supabase.auth.getSession().then(({ data: { session } }) => {
-      onAuthStateChanged(session);
-    });
-
-    void supabase.auth.onAuthStateChange((_event, session) => {
-      onAuthStateChanged(session);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (loaded) {
-      void SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded || session === undefined) {
-    return null;
-  }
+  const colorScheme = useColorScheme();
 
   return (
-    <SafeAreaView className="w-full h-full">
-      <GluestackUIProvider mode={colorScheme}>
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          {
-            session != null
-              ? (
-                  <>
-                    <Stack initialRouteName="(tabs)">
-                      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                      <Stack.Screen name="+not-found" />
-                    </Stack>
-                    <StatusBar style="auto" />
-                  </>
-                )
-              : <LoginScreen />
-          }
-        </ThemeProvider>
-      </GluestackUIProvider>
-    </SafeAreaView>
-
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
